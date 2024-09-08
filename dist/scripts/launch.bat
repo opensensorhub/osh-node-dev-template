96b34d35b0fcd54d3a3ee0cf8fcb8ff493820069
Nick Garay <nicolasgaray@icloud.com>
2024-09-08 11:00:39 -0400

    Simplify launch scripts and update submodules to latest versions

--- dist/scripts/launch.bat
+++ dist/scripts/launch.bat
@@ -1,9 +1,1 @@
-java -Xmx2g ^
-    -Dlogback.configurationFile=./config/logback.xml ^
-    -cp "lib/*" ^
-    -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" ^
-    -Djavax.net.ssl.keyStore="./config/osh-keystore.p12" ^
-    -Djavax.net.ssl.keyStorePassword="osh-keystore" ^
-    -Djavax.net.ssl.trustStore="%~dp0/config/trustStore.jks" ^
-    -Djavax.net.ssl.trustStorePassword="changeit" ^
-    org.sensorhub.impl.SensorHub ./config/config.json db
+java -Xmx2g -Dlogback.configurationFile=./config/logback.xml -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" org.sensorhub.impl.SensorHub ./config/config.json db
