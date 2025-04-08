3bd877dfab45bca6c14487a2c1a1c37b623d8bd0
Michael Elmore <michael.elmore@botts-inc.com>
2025-04-08 11:02:18 -0500

    Add missing Override annotation

--- sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Descriptor.java
+++ sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Descriptor.java
@@ -9,7 +9,6 @@
  for the specific language governing rights and limitations under the License.
 
  Copyright (C) 2020-2021 Botts Innovative Research, Inc. All Rights Reserved.
-
  ******************************* END LICENSE BLOCK ***************************/
 package com.sample.impl.sensor.drivername;
 
@@ -19,31 +18,27 @@ import org.sensorhub.api.module.ModuleConfig;
 import org.sensorhub.impl.module.JarModuleProvider;
 
 /**
- * Descriptor classes provide access to informative data on the OpenSensorHub driver
- *
- * @author your_name
- * @since date
+ * Descriptor classes provide access to informative data on the OpenSensorHub driver.
  */
 public class Descriptor extends JarModuleProvider implements IModuleProvider {
-
     /**
      * Retrieves the class implementing the OpenSensorHub interface necessary to
      * perform SOS/SPS/SOS-T operations.
      *
      * @return The class used to interact with the sensor/sensor platform.
      */
+    @Override
     public Class<? extends IModule<?>> getModuleClass() {
-
         return Sensor.class;
     }
 
     /**
-     * Identifies the class used to configure this driver
+     * Identifies the class used to configure this driver.
      *
      * @return The java class used to exposing configuration settings for the driver.
      */
+    @Override
     public Class<? extends ModuleConfig> getModuleConfigClass() {
-
         return Config.class;
     }
 }
