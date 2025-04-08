9ba45c6db575e83d30bd2fd4bdd769b759471465
Michael Elmore <michael.elmore@botts-inc.com>
2025-04-08 10:52:28 -0500

    Add comment explaining the purpose of the Activator class

--- sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Activator.java
+++ sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Activator.java
@@ -2,5 +2,10 @@ package com.sample.impl.sensor.drivername;
 
 import org.sensorhub.utils.OshBundleActivator;
 
+/**
+ * The presence of this class tells the OpenSensorHub OSGI machinery about this module.
+ * It is referenced in the 'Bundle-Activator' attribute in the OSGI section of the build.gradle file.
+ */
+@SuppressWarnings("unused")
 public class Activator extends OshBundleActivator {
 }
