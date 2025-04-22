f8f2085f3a7df3b447c1802b0ef817e2868f8b85
Michael Elmore <135364586+MichaelElmore1@users.noreply.github.com>
2025-04-22 14:13:46 -0500

    Enable sensorhub-driver-template by default in test module, add and clarify comments (#24)
    * Add comment explaining the purpose of the sensorhub-test module
    
    * Enable sensorhub-driver-template by default, remove unused dependencies
    
    * Clarify comments

--- tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
+++ tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
@@ -13,11 +13,24 @@ package org.sensorhub.node.test;
 
 import org.sensorhub.impl.SensorHub;
 
+/**
+ * The sensorhub-test module provides a convenient way to build and run an OpenSensorHub (OSH) node
+ * without requiring a full build, extraction, and launch process.
+ * This allows developers to set breakpoints in the IDE and debug the drivers directly.
+ * <p>
+ * Running this class will launch an OSH node on the address and port and with the credentials
+ * specified in the config.json file.
+ * By default, the OSH admin panel can be accessed via the web interface at localhost:8282/sensorhub/admin
+ * with the default credentials admin/admin.
+ * The API can be accessed at localhost:8282/sensorhub/api.
+ * <p>
+ * Additional drivers can be added to the build.gradle file in the dependencies section.
+ */
 public class TestSensorHub {
     private TestSensorHub() {
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SensorHub.main(new String[]{"tools/sensorhub-test/src/main/resources/config.json", "storage"});
     }
 }
