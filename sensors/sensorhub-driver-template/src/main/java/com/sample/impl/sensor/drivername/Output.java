b9656d2dba0bf72e81c9ed7bf9749db9a4c71673
Michael Elmore <michael.elmore@botts-inc.com>
2025-04-22 12:35:38 -0500

    Update copyright

--- sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Output.java
+++ sensors/sensorhub-driver-template/src/main/java/com/sample/impl/sensor/drivername/Output.java
@@ -7,7 +7,7 @@
  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  for the specific language governing rights and limitations under the License.
 
- Copyright (C) 2020-2021 Botts Innovative Research, Inc. All Rights Reserved.
+ Copyright (C) 2020-2025 Botts Innovative Research, Inc. All Rights Reserved.
  ******************************* END LICENSE BLOCK ***************************/
 package com.sample.impl.sensor.drivername;
 
--- tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
+++ tools/sensorhub-test/src/main/java/org/sensorhub/node/test/TestSensorHub.java
@@ -1,20 +1,23 @@
 /***************************** BEGIN LICENSE BLOCK ***************************
+ The contents of this file are subject to the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file, You can obtain one
+ at http://mozilla.org/MPL/2.0/.
 
- Copyright (C) 2020 Botts Innovative Research, Inc. All Rights Reserved.
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+
+ Copyright (C) 2020-2025 Botts Innovative Research, Inc. All Rights Reserved.
  ******************************* END LICENSE BLOCK ***************************/
 package org.sensorhub.node.test;
 
 import org.sensorhub.impl.SensorHub;
 
-
-public class TestSensorHub
-{
-    private TestSensorHub()
-    {        
+public class TestSensorHub {
+    private TestSensorHub() {
     }
-    
-    public static void main(String[] args) throws Exception
-    {
-        SensorHub.main(new String[] {"tools/sensorhub-test/src/main/resources/config.json", "storage"});
+
+    public static void main(String[] args) throws Exception {
+        SensorHub.main(new String[]{"tools/sensorhub-test/src/main/resources/config.json", "storage"});
     }
 }
